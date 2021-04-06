10dc09d4c7234b3b96b2cd2ac17883d7640c6aca
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-04-06 18:52:00 +0200

    Styles to the main add.js

--- src/components/adverts/AddListPage/Add.js
+++ src/components/adverts/AddListPage/Add.js
@@ -10,26 +10,40 @@ const Add = (props) => {
   const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
   return (
-    <article className="add">
       <div className="add-list-container">
         {props.addList.map((add) => {
-          return (
-            <li key={add.id}>
-              <div className="add-container">
-                <div>{add.name}</div>
-                <img src={add.photo ? `${baseUrl}${add.photo}` : NoImg} />
-                <div>{add.price.toFixed(2).replace('.', ',')} €</div>
-                <div>Categorías: {add.tags}</div>
-                <div>{add.sale ? '"Vendo"' : '"Compro"'}</div>
-                <Link to={`/advert/${add.id}`}>
+          return ( 
+            <li key={add.id} className="ad">  
+              <div className="posts-list">
+                <div className="ad-image">
+                  <div className="ad-image">
+                    <img src={add.photo ? `${baseUrl}${add.photo}` : NoImg} />
+                  </div>
+                  <div className="ad-info">
+                    <div className="ad-main-info">
+                      <p>{add.name}</p>
+                    </div>
+                    <div className="ad-state">
+                      <p>{add.sale ? 'On sale' : 'Purchase'}</p>
+                    </div>
+                    <div className="ad-decription">
+                      <p><strong>Category:</strong> {add.tags}</p>
+                    </div>
+                    <div className="ad-price">
+                      <p>{add.price} €</p> 
+                    </div>
+
+                <Link to={`/advert/{add.id}`}>
                   <Button>See in Detail</Button>
                 </Link>
+
+                  </div>
+                </div>
               </div>
             </li>
           );
         })}
       </div>
-    </article>
   );
 };
 
