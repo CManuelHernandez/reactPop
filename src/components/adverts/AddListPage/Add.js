dd01325781ca8f4b26445abcd7a47c9addcd0609
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-04-06 19:46:58 +0200

    Fixes to the add so the detail work fine again

--- src/components/adverts/AddListPage/Add.js
+++ src/components/adverts/AddListPage/Add.js
@@ -10,40 +10,39 @@ const Add = (props) => {
   const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
   return (
-      <div className="add-list-container">
+    <div className="add-list-container">
         {props.addList.map((add) => {
-          return ( 
-            <li key={add.id} className="ad">  
+          return (
+            <li key={add.id} className="ad">
               <div className="posts-list">
-                <div className="ad-image">
-                  <div className="ad-image">
-                    <img src={add.photo ? `${baseUrl}${add.photo}` : NoImg} />
-                  </div>
-                  <div className="ad-info">
-                    <div className="ad-main-info">
-                      <p>{add.name}</p>
-                    </div>
-                    <div className="ad-state">
-                      <p>{add.sale ? 'On sale' : 'Purchase'}</p>
-                    </div>
-                    <div className="ad-decription">
-                      <p><strong>Category:</strong> {add.tags}</p>
-                    </div>
-                    <div className="ad-price">
-                      <p>{add.price} €</p> 
-                    </div>
+              <div className="ad-image">
+                <img src={add.photo ? `${baseUrl}${add.photo}` : NoImg} />
+              </div>
+              <div className="ad-info">
+                <div className="ad-main-info">
+                  <p>{add.name}</p>
+                </div>
+                <div className="ad-state">
+                  <p>{add.sale ? 'On sale' : 'Purchase'}</p>
+                </div>
+                <div className="ad-decription">
+                  <p><strong>Category:</strong> {add.tags}</p>
+                </div>
+                <hr/>
+                <div className="ad-price">
+                  <p>{add.price.toFixed(2).replace('.', ',')} €</p> 
+                </div>
 
-                <Link to={`/advert/{add.id}`}>
+                <Link to={`/advert/${add.id}`}>
                   <Button>See in Detail</Button>
                 </Link>
-
-                  </div>
-                </div>
+                        
+                </div> 
               </div>
             </li>
           );
         })}
-      </div>
+    </div>
   );
 };
 
