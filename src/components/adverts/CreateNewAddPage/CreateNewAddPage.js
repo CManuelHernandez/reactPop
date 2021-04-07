ce084b6f083be4f33b8e1657a211f67a77e63b0a
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-04-07 13:37:50 +0200

    history for rerouting

--- src/components/adverts/CreateNewAddPage/CreateNewAddPage.js
+++ src/components/adverts/CreateNewAddPage/CreateNewAddPage.js
@@ -4,11 +4,15 @@ import CreateAddForm from '../CreateAddForm';
 
 import { createAdd } from '../../../api/adds';
 import Layout from '../../layout/Layout';
+import { useHistory } from 'react-router';
 
 const CreateNewAddPage = ({...props}) => {
+  const history = useHistory();
+
   const handleSubmit = (newAddData) => {
     try {
       createAdd(newAddData);
+      history.push('/');
     } catch (error) {
       console.error(error);
     }
--- src/components/auth/LoginPage/LoginPage.js
+++ src/components/auth/LoginPage/LoginPage.js
@@ -1,45 +1,60 @@
 import React from 'react';
 import LoginForm from './LoginForm';
 import { login } from '../../../api/auth';
+import { useHistory, useLocation } from 'react-router';
 import './LoginPage.css';
 
 function LoginPage({ onLogin }) {
-    const [error, setError] = React.useState(null);
-    const [isLoading, setIsLoading] = React.useState(false);
-    const isLogged = React.useRef(false);
+  const [error, setError] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const isLogged = React.useRef(false);
+  const firstTime = React.useRef(true);
   
-    const resetError = () => setError(null);
+  const resetError = React.useCallback(() => setError(), []);
+  const history = useHistory();
+  const location = useLocation();
 
-    React.useEffect(() => {
-      if (isLogged.current) {
-        onLogin();
-      }
-    }, [isLogged.current, onLogin]);
+  React.useEffect(() => {
+    if (isLogged.current) {
+      onLogin();
+      const { from } = location.state || { from: { pathname: '/' } };
+      // const from = location.state ? location.state.from : {pathname: '/'}
+
+      history.replace(from);
+    }
+  });
+
+  React.useEffect(() => {
+    if (firstTime) {
+      // Do things only the first time
+      firstTime.current = false;
+    }
+  });
   
-    const handleSubmit = async credentials => {
-      resetError();
-      setIsLoading(true);
-      try {
-        await login(credentials);
-        isLogged.current = true;
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  const handleSubmit = async credentials => {
+    resetError();
+    setIsLoading(true);
+    try {
+      await login(credentials);
+      isLogged.current = true;
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   
-    return (
-      <div className="loginPage">
-        <h1 className="loginPage-title">Log in to ReactPoP</h1>
-        <LoginForm onSubmit={handleSubmit} isLoading={isLoading} />
-        {error && (
-          <div onClick={resetError} className="loginPage-error">
-            {error.message}
-          </div>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="loginPage">
+      <h1 className="loginPage-title">Log in to ReactPoP</h1>
+      <LoginForm onSubmit={handleSubmit} isLoading={isLoading} />
+      {error && (
+        <div onClick={resetError} className="loginPage-error">
+          {error.message}
+        </div>
+      )}
+    </div>
+  );
+}
   
   export default LoginPage;
