2abb6659a6e7c3f1d71e02aef480826fc48331ca
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-06-03 11:04:20 +0200

    Fix on checkbox component

--- src/components/shared/Checkbox/Checkbox.js
+++ src/components/shared/Checkbox/Checkbox.js
@@ -2,19 +2,13 @@ import React from 'react';
 
 function Checkbox({ className, text, ...props }) {
 
-    const [name, type, disabled, checked, onChange] = Object.values({...props});
-
     return (
         <label 
             className={className}
             
         >
             <input 
-                type={type}
-                disabled={disabled}
-                name={name}
-                checked={checked}
-                onChange={onChange}
+                {...props}
             />
             {text}
         </label>
